2d1c05afb883b768279b15014e7172228094b7e1
이동욱 <jojoldu@gmail.com>
2020-07-12 22:57:42 +0900

    불필요한 코드 제거

--- src/main/java/com/jojoldu/blogcode/springbootaws/Application.java
+++ src/main/java/com/jojoldu/blogcode/springbootaws/Application.java
@@ -1,22 +1,12 @@
 package com.jojoldu.blogcode.springbootaws;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Value("${db.username}")
-    private String dbUserName;
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info(">>>>>>>>> ");
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

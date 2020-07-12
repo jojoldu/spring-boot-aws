d0045c76a09842e03611cc5462a50e7e6bc313d7
이동욱 <jojoldu@gmail.com>
2020-07-12 22:43:02 +0900

    파라미터 스토어 생성

--- src/main/java/com/jojoldu/blogcode/springbootaws/Application.java
+++ src/main/java/com/jojoldu/blogcode/springbootaws/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAwsApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAwsApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
